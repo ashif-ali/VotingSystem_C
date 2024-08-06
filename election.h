75943e244db25a9d7536951162581dd5a65d3e70
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2024-08-06 09:43:43 +0530

    Update election.h

--- election.h
+++ election.h
@@ -8,7 +8,7 @@
 // Struct Definitions
 struct currentValidID {
     int year;
-    char branch[6];
+    char branch[4]; // Adjusted to 4 to accommodate "mcs" + null terminator
     int totalVoters;
 };
 
@@ -25,22 +25,22 @@ extern int numberOfCandidates;
 extern char studentVotes[200];
 
 // Function Prototypes
-int extractYear(char userID[15]);
-int extractRollNo(char userID[15]);
-int checkBranchCode(char userID[15]);
+int extractYear(char userID[9]);
+int extractRollNo(char userID[9]);
+int checkBranchCode(char userID[9]);
 int authenticateAdmin();
 void banID();
 void createCandidateFiles();
-void deleteIllegalVote(char userID[15]);
+void deleteIllegalVote(char userID[9]);
 int getWinner();
 void initiateNewElection();
 void saveElectionInfoInFile();
 void loadElectionInfoFromFile();
 void adminPanel();
-int isValid(char userID[15]);
-int isVoted(char userID[15]);
-int isBanned(char userID[15]);
-void saveVote(char userID[15], char voteInput);
+int isValid(char userID[9]);
+int isVoted(char userID[9]);
+int isBanned(char userID[9]);
+void saveVote(char userID[9], char voteInput);
 void studentPanel();
 
 #endif // ELECTION_H
